7a0dfd4ec6b5bbfccff5ae7552b5fc2dd8819a80
Youngrock Oh <jack90@kaist.ac.kr>
2018-10-24 20:12:16 +0900

    Fix error in analytic_avg_delay(arrival_rates, service_rates, delta, A)

--- Analytic_res.py
+++ Analytic_res.py
@@ -11,6 +11,7 @@ def delay(loc_1, loc_2):
     y = loc_1[1] - loc_2[1]
     return sqrt(x * x + y * y) / c
 
+
 def delay_return(locations_source, locations_server):
     '''
 	Inputs: two layers location coordinates (array)
@@ -25,24 +26,23 @@ def delay_return(locations_source, locations_server):
             delay_matrix[i, k] = delay(locations_source[i], locations_server[k])
     return delay_matrix
 
-def analytic_avg_delay(locations_source, locations_server, arrival_rates, service_rates, A):
+
+def analytic_avg_delay(arrival_rates, service_rates, delta, A):
     '''
-	arrival_rates (array, n_{l-1} by 1 size)
-	service_rates (array, n_{l} by 1 size)
-	A: routing probabilities (array  n_{l-1} by n_{l} size)
+	arrival_rates (array, 1 by m size)
+	service_rates (array, 1 by n size)
+	A: routing probabilities (array  m by n size)
 	Output:
 	expected service time including propagation delay considering just two layers
 	'''
-    m = len(locations_source)
-    n = len(locations_server)
-    delay_matrix = delay_return(locations_source, locations_server)
-    lambda_hat = np.zeros((n, 1))
-    for j in range(n):
-        lambda_hat[j] = np.dot(arrival_rates, A[:, j])
+    m = len(arrival_rates)
+    n = len(service_rates)
+    lambda_hat = np.matmul(arrival_rates, A)
     res_sum = 0
     for i in range(m):
-        res_sum += np.dot(A[i, :], 1/(service_rates - lambda_hat.T) + delay_matrix[i, :])*arrival_rates[i]/sum(arrival_rates)
+        res_sum += np.dot(A[i, :], 1 / (service_rates - lambda_hat.T) + delta[i, :]) * arrival_rates[i] / sum(arrival_rates)
     return res_sum
+
 
 def uniform_random_network(net_region_width, net_region_height, layer_num, node_num, avg_rate_source_node):
     """
@@ -72,35 +72,36 @@ def uniform_random_network(net_region_width, net_region_height, layer_num, node_
     for i in range(layer_num - 1):
         A[i] = np.ones((node_num[i], node_num[i + 1])) / node_num[i + 1]
     A[layer_num - 1] = np.zeros((node_num[layer_num - 1], node_num[layer_num - 1]))
-
     # Construct locations, rates
     for i in range(layer_num):
         for j in range(node_num[i]):
             x = uniform(-net_region_width / 2, net_region_width / 2)
             y = uniform(-net_region_height / 2, net_region_height / 2)
-            rate = uniform(avg_rate_layer[i] * (1 - rate_sigma), avg_rate_layer[i] * (1 + rate_sigma))
+            rate = uniform(avg_rate_layer[i] * (1. - rate_sigma), avg_rate_layer[i] * (1. + rate_sigma))
             locations[i].append([x, y])
             rates[i].append(rate)
     return [locations, rates, A]
 
+
 def no_delay_optimal(arrival_rates, service_rates):
-    n1 = len(arrival_rates)
-    n2 = len(service_rates)
+    '''
+    	Find the optimal completion time using Lagrange multiplier for a network without propagation delays
+    '''
+    n = len(service_rates)
     num = 0
-    for j in range(n2):
+    for j in range(n):
         num += sqrt(service_rates[j])
     denom = sum(service_rates) - sum(arrival_rates)
     K = pow(num / denom, 2)
-    lambda_hat = np.zeros((n2, 1))
-    for j in range(n2):
+    lambda_hat = np.zeros((n, 1))
+    for j in range(n):
         lambda_hat[j] = service_rates[j] - sqrt(service_rates[j]/K)
     service_time = 0
-    for j in range(n2):
+    for j in range(n):
         service_time += lambda_hat[j] / (service_rates[j] - lambda_hat[j])
     service_time = service_time / sum(arrival_rates)
     result = {'lambda_hat': lambda_hat, 'Mean_completion_time': service_time}
     return result
-
 
 
 # Written by KJ
