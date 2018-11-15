52fe5e59850e06ac8817340ca52a64db55eb6fb9
Youngrock Oh <jack90@kaist.ac.kr>
2018-11-15 13:12:17 +0900

    Consider vol_dec for each data type
    vol_dec is an array with size (data type #, layer #)

--- Scratch_code.py
+++ Scratch_code.py
@@ -1,6 +1,10 @@
 from Analytic_res import *
-source_rates = np.array([5, 5])
-data_type_dist = np.zeros(7)
-data_type_dist[0] = 1/2
-data_type_dist[1] = 1/2
-print(avg_last_layer(data_type_dist))
+cur_layer_index = 2
+layer_dic = {0: [0, 1, 2], 1: [0, 2, 3]}
+vol_dec = np.zeros((2, 3))
+vol_dec[0, :] = np.array([1, 1/2, 1/2])
+vol_dec[1, :] = np.array([1, 1/2, 1/3])
+arrival_rates = np.array([10, 5])
+service_rates = np.array([10, 20])
+data_dist = np.array([1/2, 1/2])
+print(effective_rates(arrival_rates, service_rates, cur_layer_index, layer_dic, data_dist, vol_dec))
