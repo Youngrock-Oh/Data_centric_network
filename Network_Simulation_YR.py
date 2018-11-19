35b045989673658b7751fa9175b0b047db4eb825
Youngrock Oh <jack90@kaist.ac.kr>
2018-11-19 21:39:25 +0900

    Run the original simulation with the new simulation code (task oriented)
    If the results are the same, we can confirm that the task oriented version works correctly

--- Network_Simulation_YR.py
+++ Network_Simulation_YR.py
@@ -4,21 +4,7 @@ import Analytic_res as ar
 f1 = open("C:/Users/oe/PycharmProjects/ETRI_Data_centric_network/data_info_2.txt", 'w')
 data_total = np.zeros((4, 0))
 data_types = np.zeros((7, 3, 0))
-# data type distribution and layer_dic
-# layer_dic_input = {0: [0, 1], 1: [0, 1, 2], 2: [0, 1, 2, 3], 3: [0, 1, 2, 3, 4]}
-layer_dic_input = {0: [0, 1], 1: [0, 2], 2: [0, 1, 2], 3: [0, 1, 2, 3], 4: [0, 3], 5: [0, 2, 3], 6: [0, 1, 3]}
-data_type_dist_set = np.zeros((8, 7))
-data_type_dist_set[0, :] = 1 / 7 * np.ones(7)
-data_type_dist_set[1, :] = np.array([1/3, 1/6, 1/6, 1/12, 1/12, 1/12, 1/12])
-data_type_dist_set[2, :] = np.array([1/2, 1/8, 1/8, 1/16, 1/16, 1/16, 1/16])
-data_type_dist_set[3, :] = np.array([1/4, 1/4, 1/4, 1/16, 1/16, 1/16, 1/16])
-data_type_dist_set[4, :] = np.array([1/4, 1/8, 1/8, 1/8, 1/8, 1/8, 1/8])
-data_type_dist_set[5, :] = np.array([2/3, 1/12, 1/12, 1/24, 1/24, 1/24, 1/24])
-data_type_dist_set[6, :] = np.array([1/6, 1/3, 1/3, 1/24, 1/24, 1/24, 1/24])
-data_type_dist_set[7, :] = np.array([1/6, 1/12, 1/12, 1/6, 1/6, 1/6, 1/6])
-vol_dec_input = np.ones((7, 4))
-for i in range(7):
-    vol_dec_input[i, :] = np.array([1, 0.8, 0.8, 1])
+# Network configuration
 rates_0 = np.array([30 + 20 * (i // 5) for i in range(25)])
 rates_1 = np.array([250 + 100 * (i // 3) for i in range(9)])
 rates_2 = np.array([500, 600, 800, 900])
@@ -29,11 +15,28 @@ loc_1 = [[-8 + 8 * i, -8 + 8 * j] for i in range(3) for j in range(3)]
 loc_2 = [[-6 + 12 * i, -6 + 12 * j] for i in range(2) for j in range(2)]
 loc_3 = [[0, 0]]
 locations_input = [loc_0, loc_1, loc_2, loc_3]
+# data and task configuration
+data_task_c = {0: ["T1"], 1: ["T2"], 2: ["T1", "T2"], 3: ["T1", "T2", "T3"], 4: ["T3"], 5: ["T2", "T3"], 6: ["T1", "T3"]}
+task_vol_dec_c = {"T1": 0.8, "T2": 0.8, "T3": 1}
+data_type_dist_set = np.zeros((8, 7))
+data_type_dist_set[0, :] = 1 / 7 * np.ones(7)
+data_type_dist_set[1, :] = np.array([1/3, 1/6, 1/6, 1/12, 1/12, 1/12, 1/12])
+data_type_dist_set[2, :] = np.array([1/2, 1/8, 1/8, 1/16, 1/16, 1/16, 1/16])
+data_type_dist_set[3, :] = np.array([1/4, 1/4, 1/4, 1/16, 1/16, 1/16, 1/16])
+data_type_dist_set[4, :] = np.array([1/4, 1/8, 1/8, 1/8, 1/8, 1/8, 1/8])
+data_type_dist_set[5, :] = np.array([2/3, 1/12, 1/12, 1/24, 1/24, 1/24, 1/24])
+data_type_dist_set[6, :] = np.array([1/6, 1/3, 1/3, 1/24, 1/24, 1/24, 1/24])
+data_type_dist_set[7, :] = np.array([1/6, 1/12, 1/12, 1/6, 1/6, 1/6, 1/6])
+layer_task_c = {0: [], 1: ["T1"], 2: ["T2"], 3: ["T3"], 4: ["T4"]}
+res = NC.task_convert_input(data_task_c, task_vol_dec_c, layer_task_c)
+rate_factor_input = res[0]
+vol_dec_input = res[1]
+layer_dic_input = res[2]
 index = 1
 data_bandwidth_efficiency = np.zeros((2, 0))
 for cur_data_type_dist in data_type_dist_set:
     print("Case %d" % index)
-    result = NC.network_simulation(rates_input, locations_input, cur_data_type_dist, vol_dec_input, layer_dic_input)
+    result = NC.network_simulation(rates_input, locations_input, cur_data_type_dist, rate_factor_input, layer_dic_input)
     temp_data_info = cur_data_type_dist.__str__() + "\n"
     temp_b_e = ar.bandwidth_efficiency_compare(cur_data_type_dist, rates_0, layer_dic_input, vol_dec_input)
     temp_metric = ar.avg_sum_required_layer(cur_data_type_dist, layer_dic_input)
