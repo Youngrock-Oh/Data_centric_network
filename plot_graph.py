6370b0ee11ff9bd5f1cc53677e0a9caecc87b43c
Jisu Kim <kjsinabro@kaist.ac.kr>
2018-11-19 21:03:45 +0900

    add ticks

--- plot_graph.py
+++ plot_graph.py
@@ -47,7 +47,8 @@ plt.plot(layer2_sum, legacy2, '--s' ,label='Legacy')
 plt.xlabel('Sum of Layer2 Rates')
 plt.ylabel('Total Service Time')
 plt.legend()
-
+plt.xticks([2600, 2700, 2800, 2900, 3000])
+plt.yticks([0.0050, 0.0045, 0.0040])
 
 #vol change
 #0.64
@@ -66,10 +67,13 @@ plt.plot(vol, legacy_v1, '--s', label='Legacy')
 plt.xlabel('Decrement of Layer1')
 plt.ylabel('Total Service Time')
 plt.legend()
+plt.xticks([0.7, 0.8, 0.9])
+plt.yticks([0.0050, 0.0045, 0.0040])
 plt.figure(5)
 plt.plot(vol, efficiency_v1, '-o')
 plt.xlabel('Decrement of Layer1')
 plt.ylabel('Bandwidth Efficiency')
+plt.xticks([0.7, 0.8, 0.9])
 
 plt.figure(6)
 plt.plot(vol, uniform_v1[::-1], '-o', label='Uniform')
@@ -79,14 +83,16 @@ plt.plot(vol, legacy_v1[::-1], '--s', label='Legacy')
 plt.xlabel('Decrement of Layer2')
 plt.ylabel('Total Service Time')
 plt.legend()
+plt.xticks([0.7, 0.8, 0.9])
+plt.yticks([0.0040, 0.0045, 0.0050])
 plt.figure(7)
 plt.plot(vol, efficiency_v1[::-1], '-o')
 plt.xlabel('Decrement of Layer2')
 plt.ylabel('Bandwidth Efficiency')
-
+plt.xticks([0.7, 0.8, 0.9])
 
 #0.01
-vol2 = [0.01, 0.1, 1]
+vol2 = [0.01, 0.1,  1]
 uniform_v2 =[0.0036941, 0.00376924, 0.0051406]
 barrier_v2 =[0.00275275, 0.00281177, 0.00396074]
 proj_v2 = [0.00276193, 0.00283321, 0.00397077]
@@ -106,26 +112,32 @@ plt.plot(vol2, efficiency_v2, '-o')
 plt.xlabel('Decrement of Layer1')
 plt.ylabel('Bandwidth Efficiency')
 
+uniform_v21 = [0.0051406, 0.00376924, 0.0036941]
+barrier_v21 = [0.00396074, 0.00281177, 0.00275275]
+proj_v21 = [0.00397077, 0.00283321, 0.00276193]
+legacy_v21 = [0.00440168, 0.00461283, 0.00459566]
+efficiency_v21 = [0.715238, 0.548095, 0.57381]
+
 plt.figure(10)
-plt.plot(vol2, uniform_v2[::-1], '-o', label='Uniform')
-plt.plot(vol2, barrier_v2[::-1], ':v', label='Barrier')
-plt.plot(vol2, proj_v2[::-1], '-.^', label='Projected')
-plt.plot(vol2, legacy_v2[::-1], '--s', label='Legacy')
+plt.plot(vol2, uniform_v21, '-o', label='Uniform')
+plt.plot(vol2, barrier_v21, ':v', label='Barrier')
+plt.plot(vol2, proj_v21, '-.^', label='Projected')
+plt.plot(vol2, legacy_v21, '--s', label='Legacy')
 plt.xlabel('Decrement of Layer2')
 plt.ylabel('Total Service Time')
 plt.legend()
 plt.figure(11)
-plt.plot(vol2, efficiency_v2[::-1], '-o')
+plt.plot(vol2, efficiency_v21, '-o')
 plt.xlabel('Decrement of Layer2')
 plt.ylabel('Bandwidth Efficiency')
 
 
 #loc change
-area = [13.3*13.3, 24*24, 316.6*316.6, 469.8*469.8]
-uniform_l = [0.00453855, 0.00496639, 0.0164602, 0.0225108]
-barrier_l = [0.00368161, 0.00387546, 0.009280803, 0.0118633]
-proj_l = [0.00369384, 0.00389243, 0.00920971, 0.0118551]
-legacy_l = [0.004297, 0.00459959, 0.0112205, 0.0147556]
+area = [13.3*13.3, 24*24, 223.6*223.6, 316.6*316.6, 387.3*387.3, 469.8*469.8]
+uniform_l = [0.00453855, 0.00496639, 0.0128247, 0.0164602, 0.0192825, 0.0225108]
+barrier_l = [0.00368161, 0.00387546, 0.00756083, 0.009280803, 0.0104368, 0.0118633]
+proj_l = [0.00369384, 0.00389243, 0.00755169, 0.00920971, 0.0104517, 0.0118551]
+legacy_l = [0.004297, 0.00459959, 0.0091127, 0.0112205, 0.0128381, 0.0147556]
 
 plt.figure(12)
 plt.plot(area, uniform_l, '-o', label='Uniform')
@@ -135,6 +147,7 @@ plt.plot(area, legacy_l, '--s', label='Legacy')
 plt.xlabel('Area(km^2)')
 plt.ylabel('Total Service Time')
 plt.legend()
+plt.yticks([0.0050, 0.0100, 0.0150, 0.0200])
 
 
 
